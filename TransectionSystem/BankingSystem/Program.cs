09db2804f0248b30aea51223b7798dd1f5d1b948
Koushiq <koushiq1234@gmail.com>
2019-11-07 18:12:33 +0600

    initial commit

---
+++ TransectionSystem/BankingSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account[] accounts = new Account[4];
+            accounts[0] = new SavingsAccount("A-203", "Bruce Wayne", new DateTime(2019, 11, 22),2000,"5");
+            accounts[1] = new SavingsAccount("A-204", "Harvey Dent", new DateTime(2019, 11, 22), 1500, "5");
+            accounts[2] = new CurrentAccount("A-205", "Alfred Pennywort", new DateTime(2019, 11, 22), 3000);
+            accounts[3] = new CurrentAccount("A-206", "Oracle Gordon", new DateTime(2019, 11, 22), 200);
+
+            foreach(Account acc in accounts)
+            {
+                acc.ShowInfo();
+            }
+
+            accounts[0].Deposit(500);
+            accounts[0].ShowInfo();
+            accounts[1].Withdraw(200);
+            accounts[1].ShowInfo();
+
+            Account.Transfer(accounts[2], accounts[1], 3000);
+
+            foreach (Account acc in accounts)
+            {
+                acc.ShowInfo();
+            }
+
+
+        }
+    }
+}
---
+++ TransectionSystem/BankingSystem/SavingsAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    class SavingsAccount : Account
+    {
+        private string savingsDuration;
+        public SavingsAccount(string id, string name, DateTime date, double balance,string savingsDuration) : base(id,name,date,balance)
+        {
+            this.SavingsDuration = savingsDuration;
+        }
+        public string SavingsDuration
+        {
+            get
+            {
+                return this.savingsDuration;
+            }
+            set
+            {
+                this.savingsDuration = value ; 
+            } 
+        }
+
+        public override bool Withdraw(double amount)
+        {
+            if (amount>=0 && this.Balance-amount>500)
+            {
+                this.Balance -= amount;
+                Console.WriteLine("Withdraw Successful");
+                return true;
+            }
+            ShowHaultMessage();
+            return false;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Account Savings Duration "+this.SavingsDuration+" year\n");
+        }
+
+
+    }
+}
